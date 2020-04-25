6dd468753dd5394aff1479963c1cd95b1d72867e
Saurabh Suresh <saurabhsuresh4s@gmail.com>
2020-04-25 17:27:01 +0400

    Quotes change everytime Sos Card added

--- app/src/main/java/com/forevercoders/pink/MainActivity.java
+++ app/src/main/java/com/forevercoders/pink/MainActivity.java
@@ -1,14 +1,24 @@
 package com.forevercoders.pink;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] quotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quotes = getResources().getStringArray(R.array.quotes);
+        int randomIndex = new Random().nextInt(quotes.length);
+        String randomName = quotes[randomIndex];
+        TextView quotestxt = (TextView) findViewById(R.id.quotestxt);
+        quotestxt.setText(randomName);
     }
 }
